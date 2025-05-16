c19a57084747a4280223514e599f9504b380c907
Muskan-7867 <muskanloach984@gmail.com>
2025-05-16 10:35:35 +0530

    new

--- src/layout/UserLayout.tsx
+++ src/layout/UserLayout.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/common/navbar/Navbar";
 import NewFooter from "../components/common/footer/NewFooter";
+import AuthProvider from "./auth/AuthProvider";
 
 const UserLayout: React.FC = () => {
   return (
-    <div>
+    <AuthProvider>
       <Navbar />
       <Outlet />
       <NewFooter />
-    </div>
+    </AuthProvider>
   );
 };
 
