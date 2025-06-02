108673327a21af94cb20e4242222f3e6fe720591
Muskan-7867 <muskanloach984@gmail.com>
2025-06-02 12:48:52 +0530

    fix order table

--- src/pages/admin/order/components/OrderTableForAdmin.tsx
+++ src/pages/admin/order/components/OrderTableForAdmin.tsx
@@ -5,13 +5,11 @@ import TableData, { Column } from "./TableData";
 import { fetchOrdersQuery } from "../../../../services/queries";
 import { OrderItem } from "../../../../types/Product";
 import Pagination from "../../../user/products/components/Pagination";
-
 export interface Payment {
   amount: number;
   paymentMethod: string;
   paymentStatus: string;
 }
-
 export interface Order {
   quantity: number;
   totalPrice: number;
@@ -29,14 +27,7 @@ const OrderTableForAdmin = () => {
   const [page] = useQueryState("page", { defaultValue: "1" });
   const currentPage = Number(page);
   const itemsPerPage = 10;
-
-  const {
-    data: orders = [],
-    isLoading,
-    error
-  } = useQuery<Order[]>(fetchOrdersQuery());
-  console.log("from ordertableee", orders);
-
+  const {  data: orders = [], isLoading, error } = useQuery<Order[]>(fetchOrdersQuery());
   const [localOrders, setLocalOrders] = useState<Order[]>([]);
 
   useEffect(() => {
@@ -49,11 +40,9 @@ const OrderTableForAdmin = () => {
       <p className="p-4 text-center">You have not placed any orders yet.</p>
     );
   }
-
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentOrders = localOrders.slice(indexOfFirstItem, indexOfLastItem);
-
   const columns: Column<Order>[] = [
     {
       label: "Order Items",
@@ -76,17 +65,11 @@ const OrderTableForAdmin = () => {
           {order.orderItems.map((item, i) => {
             const product = item.product;
             return typeof product === "object" && product?.images?.length ? (
-              <img
-                key={i}
-                src={product.images[0].url}
-                alt="Product"
+              <img key={i} src={product.images[0].url} alt="Product"
                 className="lg:w-12 lg:h-12 md:w-16 md:h-10 sm:w-14 sm:h-8 w-18 h-10 object-cover rounded border bg-white"
               />
             ) : (
-              <div
-                key={i}
-                className="w-12 h-12 flex items-center justify-center bg-gray-100 border text-xs text-gray-500 rounded"
-              >
+              <div  key={i} className="w-12 h-12 flex items-center justify-center bg-gray-100 border text-xs text-gray-500 rounded" >
                 No Image
               </div>
             );
@@ -112,9 +95,7 @@ const OrderTableForAdmin = () => {
           );
           setLocalOrders(updated);
         };
-
         const selected = order.status;
-
         return (
           <select
             className={`px-3 py-1 rounded-md outline-none ${
