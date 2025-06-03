a1bb2695aac79628563ab1c501b9f3914530ac0d
Muskan-7867 <muskanloach984@gmail.com>
2025-06-03 13:04:20 +0530

    update

--- src/services/razorpayforcartorder.ts
+++ src/services/razorpayforcartorder.ts
@@ -73,34 +73,28 @@ export const initiateRazorpayPayment = async ({
       throw new Error("Invalid response from server");
     }
 
-    const { id:razorpayOrderId, amount } = data.razorpayOrder;
+    const { id, amount } = data.razorpayOrder;
     const orderId = data.order._id;
-
-    // Store these values for later use in the handler
-    const backendRazorpayOrderId = razorpayOrderId;
-    const backendOrderId = orderId;
 
     const options = {
       key: RAZORPAY_KEY,
       amount,
       currency: "INR",
-      order_id: razorpayOrderId,
+      order_id: id,
       name: "OMEG-BAZAAR",
       description: `Payment for ${products.length} item${products.length > 1 ? 's' : ''}`,
       handler: async (response: RazorpayResponse) => {
         const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
           response;
-        console.log("Razorpay payment response:", response);
-        console.log("Backend Order ID: backendOrderId and razorpay_order_id", backendOrderId, razorpay_order_id);
-        console.log("Backend Razorpay Order ID:", backendRazorpayOrderId);
+        console.log("from frontend payment " , )
         try {
           const verification = await axios.post(
             `${BASE_URL}/api/v1/order/paymentverify`,
             {
-              razorpay_order_id: backendRazorpayOrderId,
+              razorpay_order_id,
               razorpay_payment_id,
               razorpay_signature,
-              orderId : backendOrderId,
+              orderId,
               paymentMethod: paymentmethod
             },
             {
@@ -108,7 +102,7 @@ export const initiateRazorpayPayment = async ({
             }
           );
 
-          if (verification.data.success) {
+          if (verification?.data.success) {
             setShowSuccessPopup(true);
             setTimeout(() => {
               navigate("/products");
