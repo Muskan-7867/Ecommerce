c6a8dce81273610e4501832dad7bd50355fa00e1
Muskan-7867 <muskanloach984@gmail.com>
2025-06-03 13:06:08 +0530

    fixx

--- src/services/razorpayforcartorder.ts
+++ src/services/razorpayforcartorder.ts
@@ -86,7 +86,7 @@ export const initiateRazorpayPayment = async ({
       handler: async (response: RazorpayResponse) => {
         const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
           response;
-        console.log("from frontend payment " , )
+        console.log("from frontend payment " , response)
         try {
           const verification = await axios.post(
             `${BASE_URL}/api/v1/order/paymentverify`,
