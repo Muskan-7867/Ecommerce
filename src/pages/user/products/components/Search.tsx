fe97528261766e44916f380dbe80c399788ef681
Muskan-7867 <muskanloach984@gmail.com>
2025-06-09 15:24:46 +0530

    responsive search bar

--- src/pages/user/products/components/Search.tsx
+++ src/pages/user/products/components/Search.tsx
@@ -6,19 +6,20 @@ const Search = () => {
   const [search, setSearch] = useQueryState("search");
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-  setSearch(e.target.value)
+    setSearch(e.target.value);
   };
 
   return (
-    <div className=" lg:mt-12 mt-8 lg:ml-8 ml-2">
-      <div className="relative w-[90vw]">
-        <FaSearch className="absolute left-3 top-3 text-gray-400" />
+    <div className="w-full sm:px-6 mt-6 sm:mt-8 lg:mt-10">
+      <div className="relative w-full">
+        <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
         <input
           type="text"
-          placeholder="Search item by name"
-          value={search || ''}
+          placeholder="Search Product by name"
+          value={search || ""}
           onChange={handleSearch}
-          className="w-full p-2 pl-10 bg-red-50 rounded-full focus:outline-none"
+          className="w-full p-2 pl-10 bg-red-50 rounded-full focus:outline-none
+                     text-sm sm:text-base md:p-3 md:pl-12"
         />
       </div>
     </div>
